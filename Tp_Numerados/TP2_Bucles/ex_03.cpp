33ca8262f52ce7af43c3fefd9a8a870c93e1b82f
w/berni <professionalbern@gmail.com>
2026-06-03 09:54:25 -0300

    Actualizar ex_03.cpp con un bucle de repeticion

--- Tp_Numerados/TP2_Bucles/ex_03.cpp
+++ Tp_Numerados/TP2_Bucles/ex_03.cpp
@@ -1,8 +1,19 @@
-    int positivos=0, negativos=0, neutros=0;
-    for(int i=0;i<10;i++){
-        cin >> num;
-        if(num>0) positivos++;
-        else if(num<0) negativos++;
-        else neutros++;
-    }
-    cout << "Positivos: " << positivos << " Negativos: " << negativos << " Neutros: " << neutros << endl;
+#include <iostream>
+
+int main() {
+    int num;
+    char repetir;
+    do {
+        int positivos=0, negativos=0, neutros=0;
+        for(int i=0;i<10;i++){
+            std::cin >> num;
+            if(num>0) positivos++;
+            else if(num<0) negativos++;
+            else neutros++;
+        }
+        std::cout << "Positivos: " << positivos << " Negativos: " << negativos << " Neutros: " << neutros << std::endl;
+        std::cout << "Desea repetir? (s/n): ";
+        std::cin >> repetir;
+    } while(repetir == 's' || repetir == 'S');
+    return 0;
+}
