5de1f9ec359e34cab3bfeff14b4d4b1956bf26f2
w/berni <professionalbern@gmail.com>
2026-06-03 09:56:01 -0300

    Actualizar ex_04.cpp con bucle de repeticion

--- Tp_Numerados/TP2_Bucles/ex_04.cpp
+++ Tp_Numerados/TP2_Bucles/ex_04.cpp
@@ -2,14 +2,19 @@
 #include <vector>
 
 int main() {
-    std::vector<int> notas(10);
-    int suma=0, peorNota=10;
-    for(int i=0;i<10;i++){
-        std::cin >> notas[i];
-        suma += notas[i];
-        if(notas[i] < peorNota) peorNota = notas[i];
-    }
-    double promedio = suma / 10.0;
-    std::cout << promedio << " " << peorNota << std::endl;
+    char repetir;
+    do {
+        std::vector<int> notas(10);
+        int suma=0, peorNota=10;
+        for(int i=0;i<10;i++){
+            std::cin >> notas[i];
+            suma += notas[i];
+            if(notas[i] < peorNota) peorNota = notas[i];
+        }
+        double promedio = suma / 10.0;
+        std::cout << promedio << " " << peorNota << std::endl;
+        std::cout << "Desea repetir? (s/n): ";
+        std::cin >> repetir;
+    } while(repetir == 's' || repetir == 'S');
     return 0;
 }
